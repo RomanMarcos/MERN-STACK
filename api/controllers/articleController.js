0bdac8218c17279c92210c08291707d32782e66d
marcos <marcoss.-08@hotmail.com>
2024-03-02 16:37:36 -0300

    feat(04): implementing article endpoints

--- api/controllers/articleController.js
+++ api/controllers/articleController.js
@@ -1,8 +1,9 @@
 const validator = require('validator');
 const articleModel = require('../models/Article');
+const fs = require('fs');
 
 const newArticle = (req, res) => {
-
+    
     const { title, content, date, image } = req.body;
 
     try {
@@ -32,8 +33,143 @@ const newArticle = (req, res) => {
             message: 'There was an error trying to create a new article because of missing required information (title or content)..'
         });
     }
+
+}
+
+const getArticles = (req, res) => {
+
+    const { limit } = req.params;
+
+    articleModel.find({})
+        .limit(limit)
+        .sort({date: -1})
+        .then((articles) => {
+            return res.status(200).json({
+                status: 'Success',
+                articles
+            });
+        })
+        .catch(() => {
+            return res.status(404).json({
+                status: 'Error',
+                message: 'There are no articles in the database..'
+            });
+        });
+    
+}
+
+const getArticle = (req, res) => {
+
+    const { id } = req.params;
+
+    articleModel.findById(id)
+    .then((article) => {
+        return res.status(200).json({
+            status: 'Success',
+            article
+        });
+    })
+    .catch(() => {
+        return res.status(404).json({
+            status: 'Error',
+            message: 'There are no article in the database with that exact ID..'
+        });
+    });
+
+}
+
+const deleteArticle = (req, res) => {
+
+    const { id } = req.params;
+
+    articleModel.deleteOne({ _id: id })
+    .then(() => {
+        return res.status(200).json({
+            status: 'Success',
+            message: 'The article was successfully removed from the database..'
+        });
+    })
+    .catch(() => {
+        return res.status(404).json({
+            status: 'Error',
+            message: 'There was an error trying to delete the article..'
+        });
+    });
+
+}
+
+const editArticle = (req, res) => {
+
+    const { id } = req.params;
+    const { title, content, date, image } = req.body;
+
+    try {
+
+        if( validator.isEmpty(title) || validator.isEmpty(content) ){
+            throw new Error();
+        }
+
+        articleModel.findOneAndUpdate({ _id: id }, { title, content }, {new: true})
+        .then((article) => {
+            return res.status(200).json({
+                status: 'Success',
+                message: 'The article was successfully edited..',
+                article: article
+            });
+        })
+        .catch(() => {
+            return res.status(404).json({
+                status: 'Error',
+                message: 'There was an error trying to edit the article..'
+            });
+        });
+
+    } catch(error) {
+        return res.status(400).json({
+            message: 'There was an error trying to edit the article..'
+        });
+    }
+
+}
+
+const upload = (req, res) => {
+
+    let file = req.file.originalname;
+
+    let fileplit = file.split('\.');
+    let extension = fileplit[1];
+
+    if (extension != 'png' && extension != 'jpg' && extension != 'jpeg' && extension != 'gif') {
+        fs.unlink(req.file.path, (error) => {
+            return res.status(400).json({
+                message: "Invalid file"
+            });
+        });
+    }
+
+    const {id} = req.params;
+
+    articleModel.findOneAndUpdate({ _id: id }, { image: req.file.filename }, {new: true})
+        .then((article) => {
+            return res.status(200).json({
+                status: 'Success',
+                message: 'The article was successfully edited..',
+                article: article
+            });
+        })
+        .catch(() => {
+            return res.status(404).json({
+                status: 'Error',
+                message: 'There was an error trying to edit the article..'
+            });
+        });
 }
 
 module.exports = {
-    newArticle
+    newArticle,
+    getArticles,
+    getArticle,
+    deleteArticle,
+    editArticle,
+    upload
 }
--- api/routes/articleRoutes.js
+++ api/routes/articleRoutes.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+const storage= multer.diskStorage({
+    destination: function(req, file, cb) {
+        // cb method allow me to define where the files will be storage, in this case inside the /images/articles/ folder
+        cb(null, './images/articles/');
+    }, 
+    __filename: function(req, file, cb) {
+        cb(null, 'article' + Date.now() + file.originalname());
+    }
+});
+
+const uploads = multer({storage: storage});
 
 const articleController = require('../controllers/articleController');
 
 router.post('/article/new', articleController.newArticle);
+router.get('/articles/:limit?', articleController.getArticles);
+router.get('/article/:id', articleController.getArticle);
+router.delete('/deleteArticle/:id', articleController.deleteArticle);
+router.put('/edit/:id', articleController.editArticle);
+//The second params on the following route definition is a Middleware that actually will execute BEFORE the upload method of the controller
+router.post('/uploadImg/:id', [uploads.single('file')], articleController.upload);
 
 module.exports = router;
