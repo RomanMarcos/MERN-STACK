06b23a62c241d46fb218925d6ab374c1fbf47d55
marcos <marcoss.-08@hotmail.com>
2024-03-09 18:10:25 -0300

    feat(14): navBar implementation

--- blog-webapp/src/components/navBar/NavBar.jsx
+++ blog-webapp/src/components/navBar/NavBar.jsx
@@ -1,6 +1,23 @@
+import { SearchBox } from '../searchBox/SearchBox';
+import './navBar.scss';
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+      <div className='navBar-component'>
+        <div className='navBar-seccion'>
+          <ul className='navBar-ul'>
+            <li className='navBar-li border-right'> <a href='#' className='navBar-link'> Articles </a> </li>
+            <li className='navBar-li'> <a href='#' className='navBar-link'> New article </a> </li>
+          </ul>
+        </div>
+        <div className='navBar-seccion'>
+          <a href='#' className='navBar-link'><img className='navBar-img' src='/twiter.png' alt='twiter' /></a>
+          <a href='#' className='navBar-link'><img className='navBar-img' src='/linkedin.png' alt='linkedin' /></a>
+          <a href='#' className='navBar-link'><img className='navBar-img' src='/facebook.png' alt='facebook' /></a>
+        </div>
+        <SearchBox />
+      </div>
+    </>
   )
 }
