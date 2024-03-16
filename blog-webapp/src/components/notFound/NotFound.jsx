ca3acca717e348ac965ff2bcaf9dafb4b78554cd
marcos <marcoss.-08@hotmail.com>
2024-03-16 16:38:44 -0300

    feat(searchArticle): not found page & styles

--- blog-webapp/src/components/notFound/NotFound.jsx
+++ blog-webapp/src/components/notFound/NotFound.jsx
@@ -1,7 +1,11 @@
-
+import './notFound.scss';
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='notFound-container'>
+      <div className="notFound-content">
+        Oops! It seems the article you are searching does not exist.
+      </div>
+    </div>
   )
 }
