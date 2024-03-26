0dffc60e34e4537e23f84e0e1731a04c390d4dda
Marcos Roman <marcoss.-08@hotmail.com>
2024-03-26 12:38:00 -0300

    feat(docker): adding docker file for services: api, blog and mongodb

--- api/database/connection.js
+++ api/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connection = async() => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+        await mongoose.connect("mongodb://blog-db:27017/blogDB");
         console.log('DB connection successfully!');
     } catch(error) {
         console.log(`There was an error trying to connect to the DB ${error}`);
